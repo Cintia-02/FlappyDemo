20093140ae59dfebc3d1770d338936b1d8814498
gloriane <glorianetran@gmail.com>
2017-11-02 15:56:54 -0700

    bird class and play state

--- FlappyDemo/core/src/com/missionbit/game/states/PlayState.java
+++ FlappyDemo/core/src/com/missionbit/game/states/PlayState.java
@@ -1,8 +1,10 @@
 package com.missionbit.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.missionbit.game.FlappyDemo;
+import com.missionbit.game.sprites.Bird;
 
 /**
  * Created by missionbit on 10/24/17.
@@ -10,34 +12,41 @@ import com.missionbit.game.FlappyDemo;
 
 public class PlayState extends State {
 
-    private Texture bird = new Texture("bird.png");
+    private Bird bird;
+    private Texture bg;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2,
                 FlappyDemo.HEIGHT / 2);
+        bird = new Bird(50, 100);
+        bg = new Texture("bg.png");
     }
 
     @Override
     protected void handleInput() {
-
+        if(Gdx.input.justTouched()){
+            bird.jump();
+        }
     }
 
     @Override
     public void update(float dt) {
-
+        handleInput();
+        bird.update(dt);
     }
 
     @Override
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
-        sb.draw(bird, 50, 50);
+        sb.draw(bg,(cam.position.x - cam.viewportWidth / 2), 0);
+        sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
         sb.end();
     }
 
     @Override
     public void dispose() {
-
+        bg.dispose();
     }
 }
